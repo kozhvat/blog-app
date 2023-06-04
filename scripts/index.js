9279d532139e74386d6c50e026be1f8dadf53831
kozhvat <kozhvat@mail.ru>
2023-06-04 12:55:42 +0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// ПЕРЕМЕННЫЕ
+const inputAddNewPostNode = document.querySelector(
+  '[data-find="input-add-new-post"]'
+);
+
+// -------------------------------------------------------------------------------
+
+// ПЕРВИЧНЫЙ КОД ПРИ ОТКРЫТИИ СТРАНИЦЫ
+init('???????????????????');
+
+function init() {}
+// -------------------------------------------------------------------------------
+
+// ОБРАБОТЧИК НАЖАТИЯ КНОПКИ "ДОБАВИТЬ"
+buttonAddExpenseNode.addEventListener('click', azazazazaza);
+
+// ОБРАБОТЧИК НАЖАТИЯ КНОПКИ СБРОСА ТРАТ И ОБНОВЛЕНИЯ ВСЕГО
+buttonResetExpensesNode.addEventListener('click', azazazaz);
+
+// ОБРАБОТЧИК НАЖАТИЯ КНОПКИ "ИЗМЕНИТЬ ЛИМИТ"
+buttonChangeLimitNode.addEventListener('click', azazazazazazaza);
